8e5df0c66d21a9019f65c7a8add80e4df9bc32bf
ashrafff452 <ashrafff452@gmail.com>
2026-07-06 17:44:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "demo" {
-
-  ami           = "ami-0521cb2d60cfbb1a6"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform-jenkins"
-  }
+resource "local_file" "my_pet" {
+filename = "pets.txt"
+content  = "I love pets!"
 }
