77016305e67b21923d6022ff8580e25034550d9a
ashrafff452 <ashrafff452@gmail.com>
2026-07-08 15:38:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "local_file" "my_pet" {
     filename = var.filename
-    content = var.content 
+    content = "My cat name is ${random_pet.petname.id}|" 
+}
+resource "random_pet" "petname" {
+  prefix = "MR"
+  separator = "."
+  length = "1"
 }
